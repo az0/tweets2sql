1e53f66699875d5255a2467743025e91dc54cb6f
Andrew Ziem <ahz001@gmail.com>
2013-01-29 11:35:03 -0700

    Integrate robust error handling.  Make timeline archiver a class.

--- twitter2sql.py
+++ twitter2sql.py
@@ -19,13 +19,18 @@
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+from sqlobject import *
+from sqlobject.dberrors import IntegrityError
+from twitter.api import TwitterError
+from twitter.util import Fail, err
+import httplib
 import os
 import time
 import twitter
-from sqlobject import *
-from sqlobject.dberrors import IntegrityError
+import urllib2
 
-conection = None
+Q_TIMELINE = 1
+Q_SEARCH = 2
 
 class SearchTweet(SQLObject):
     """A tweet as found in the Twitter search API"""
@@ -122,49 +127,122 @@ def search(query_str):
         if len(tquery['results']) < RPP:
             break
 
+class TimelineArchiver:
+    """Archive a user's timeline"""
+    def __init__(self, screen_name):
+        self.screen_name = screen_name
+        # find the most recent ID
+        results = Timeline.selectBy(screen_name = screen_name)
+        if 0 == results.count():
+            # make a new timeline
+            self.timeline = Timeline(screen_name = screen_name)
+            self.max_id = None
+        else:
+            # use the existing timeline
+            self.timeline = results[0]
+            # find max ID
+            self.max_id = Timeline.select(Timeline.q.screen_name==screen_name)[0].max_id
 
-def timeline_sub(tl, timeline):
-    for tweet in tl:
-        created_at = twitterdate(tweet['created_at'])
-        kwargs = { 'id' : tweet['id'], \
-            'user_id' : tweet['user']['id'], \
-            'user_screen_name' : tweet['user']['screen_name'], \
-            'in_reply_to_status_id' : tweet['in_reply_to_status_id'], \
-            'in_reply_to_user_id' : tweet['in_reply_to_user_id'], \
-            'in_reply_to_screen_name' : tweet['in_reply_to_screen_name'], \
-            'text' : tweet['text'], \
-            'source' : tweet['source'], \
-            'created_at' : created_at, \
-            'timeline' : timeline }
-        try:
-            TimelineTweet(**kwargs)
-        except IntegrityError:
-            print 'DEBUG: tweet already in database', tweet['id']
+        # connection
+        self.twitter = twitter.Twitter(auth=twitter.NoAuth(), api_version='1', domain='api.twitter.com')
+
+        # stats
+        self.new = 0
+        self.dup = 0
+
+
+    def query(self):
+        """Make one API call and archive the results"""
+        print 'TimelineArchiver.query()'
+        kwargs = { 'screen_name' : self.screen_name, 'rpp': 200 }
+        if self.max_id:
+            kwargs['max_id'] = self.max_id
+        tl = self.twitter.statuses.user_timeline(**kwargs)
+        for tweet in tl:
+            created_at = twitterdate(tweet['created_at'])
+            kwargs = { 'id' : tweet['id'], \
+                'user_id' : tweet['user']['id'], \
+                'user_screen_name' : tweet['user']['screen_name'], \
+                'in_reply_to_status_id' : tweet['in_reply_to_status_id'], \
+                'in_reply_to_user_id' : tweet['in_reply_to_user_id'], \
+                'in_reply_to_screen_name' : tweet['in_reply_to_screen_name'], \
+                'text' : tweet['text'], \
+                'source' : tweet['source'], \
+                'created_at' : created_at, \
+                'timeline' : self.timeline }
+            try:
+                TimelineTweet(**kwargs)
+            except IntegrityError:
+                print 'DEBUG: tweet already in database', tweet['id']
+                self.dup += 1
+            else:
+                self.new += 1
 
 
 
-def timeline(screen_name):
-    print '*** SCREEN NAME: %s' % screen_name
 
-    # find the most recent ID
-    results = Timeline.selectBy(screen_name = screen_name)
-    if 0 == results.count():
-        # make a new timeline
-        timeline = Timeline(screen_name = screen_name)
+def archive(twitter, q_type, q_str):
+    if q_type == Q_TIMELINE:
+        return timeline(q_str, max_id)
+    elif q_type == Q_SEARCH:
+        return search(q_str, max_id)
     else:
-        # use the existing timeline
-        timeline = results[0]
+        raise RuntimeError('unknown query type %d: %s' % (q_type, q_str))
 
-    # prepare to search
-    t = twitter.Twitter(auth=twitter.NoAuth(), api_version='1', domain='api.twitter.com')
-    kwargs = { 'screen_name' : screen_name }
+
+
+def archive_loop(archiver):
+    """Generic loop and handling for all kinds of archiving.
+    Mostly copied from Mike Verdone's twitter.archiver."""
+    fail = Fail()
+    twitter = archiver.twitter
+    last_new = 0
+    # download one API call at a time until done while handling errors
     while True:
-        tl = t.statuses.user_timeline(**kwargs)
-        timeline_sub(tl, timeline)
-        kwargs['max_id'] = min([tweet['id'] for tweet in tl])
-        max_id = max(max_id, max([tweet['id'] for tweet in tl]))
-    # if the search succeeded, save the most recent ID
-    timeline.max_id = max_id
+        try:
+            archiver.query()
+        except TwitterError as e:
+            if e.e.code == 401:
+                err("Fail: %i Unauthorized (tweets of that user are protected)"
+                    % e.e.code)
+                break
+            elif e.e.code == 400:
+                err("Fail: %i API rate limit exceeded" % e.e.code)
+                rate = twitter.account.rate_limit_status()
+                reset = rate['reset_time_in_seconds']
+                reset = time.asctime(time.localtime(reset))
+                delay = int(rate['reset_time_in_seconds']
+                            - time.time()) + 5 # avoid race
+                err("Hourly limit of %i requests reached, next reset on %s: "
+                    "going to sleep for %i secs" % (rate['hourly_limit'],
+                                                    reset, delay))
+                fail.wait(delay)
+                continue
+            elif e.e.code == 404:
+                err("Fail: %i This profile does not exist" % e.e.code)
+                break
+            elif e.e.code == 502:
+                err("Fail: %i Service currently unavailable, retrying..."
+                    % e.e.code)
+            else:
+                err("Fail: %s\nRetrying..." % str(e)[:500])
+            fail.wait(3)
+        except urllib2.URLError as e:
+            err("Fail: urllib2.URLError %s - Retrying..." % str(e))
+            fail.wait(3)
+        except httplib.error as e:
+            err("Fail: httplib.error %s - Retrying..." % str(e))
+            fail.wait(3)
+        except KeyError as e:
+            err("Fail: KeyError %s - Retrying..." % str(e))
+            fail.wait(3)
+        else:
+            this_new = archiver.new - last_new
+            err('Browsing.  New:%d.  Dup:%d' % (archiver.new, archiver.dup))
+            if this_new < 190:
+                break
+            last_new = archiver.new
+            fail = Fail()
 
 
 def main():
@@ -174,15 +252,22 @@ def main():
     parser.add_option('-q', dest='query', type="string", help='Archive search results such as #foo')
     parser.add_option('-s', dest='screen_name', type="string", help='Archive timeline for given screen name')
     (options, args) = parser.parse_args()
+
+    # setup SQLObject
     connection = connectionForURI(options.connection_string)
     sqlhub.processConnection = connection
     Query.createTable(ifNotExists = True)
     SearchTweet.createTable(ifNotExists = True)
     TimelineTweet.createTable(ifNotExists = True)
     Timeline.createTable(ifNotExists = True)
+
+    # process command line
     if options.query:
+        print '*** SEARCH: %s' % options.query
         search(options.query)
     if options.screen_name:
-        timeline(options.screen_name)
+        print '*** SCREEN NAME: %s' % options.screen_name
+        ta = TimelineArchiver(options.screen_name)
+        archive_loop(ta)
 
 main()
